b3f2d1351807574cb2359f541bf1b86f91825704
Harsh chawla <harsh1560.be20@chitkara.edu.in>
2023-04-07 14:48:57 +0530

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Navbar from './comps/NavbarComponent/Navbar';
+import Home from './comps/HomeComponent/Home';
+import Service from './comps/ServicesHomeComponent/Service';
+import Contacts from './comps/ContactComponent/Contact';
 
 function App() {
   return (
-    <div></div>
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+      <Service></Service>
+      <Contacts></Contacts>
+    </>
   );
 }
 

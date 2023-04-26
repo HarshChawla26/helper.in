e963e65ff20760d2c0dae73ed97899fb68b617fc
Harsh chawla <78164558+HarshChawla26@users.noreply.github.com>
2023-04-26 19:57:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,20 @@
-import "./App.css";
-import Navbar from "./comps/NavbarComponent/Navbar";
-import Home from "./comps/HomeComponent/Home";
-import Service from "./comps/ServicesHomeComponent/Service";
-import Contacts from "./comps/ContactComponent/Contact";
-import { Navigate, Route, Routes } from "react-router";
-import Results from "./comps/ResultsComponent/Results";
-import Auth from "./comps/Auth/Auth";
+import './App.css';
+import Navbar from './comps/NavbarComponent/Navbar';
+import Home from './comps/HomeComponent/Home';
+import Service from './comps/ServicesHomeComponent/Service';
+import Contacts from './comps/ContactComponent/Contact';
+import { Route, Routes } from 'react-router';
+import Results from './comps/ResultsComponent/Results';
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Home />
-              <Service />
-            </>
-          }
-        />
-        <Route path="/results" element={<Results />} />
-        <Route path="/auth/*" element={<Auth />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path='/' element={<><Home/><Service /><Contacts /></>}></Route>
+        <Route path='/results' element={<><Results/><Contacts /></>}></Route>
+        {/* <Route path='/auth' element={<Auth/>}></Route> */}
       </Routes>
-
-      {/* <Contacts /> */}
     </>
   );
 }

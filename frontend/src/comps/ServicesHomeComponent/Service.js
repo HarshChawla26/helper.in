119adc6a4bbffc796896d5fefc57fa23a2f74897
Gautam-2002 <gautam2sethi@gmail.com>
2023-05-14 01:01:43 +0530

    ui 1.1.1.1

--- frontend/src/comps/ServicesHomeComponent/Service.js
+++ frontend/src/comps/ServicesHomeComponent/Service.js
@@ -10,11 +10,31 @@ export default function Service() {
 
       <section>
         {/* Card Wrapper */}
-        <div>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="src-cards-4">
+          <Card
+            classs={"serc-img-01"}
+            imgsrc={
+              "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_568/t_high_res_template/images/growth/home-screen/1678721781429-69c225.jpeg"
+            }
+          />
+          <Card
+            classs={"serc-img-02"}
+            imgsrc={
+              "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_568/t_high_res_template/images/growth/home-screen/1678463313266-174e91.jpeg"
+            }
+          />
+          <Card
+            classs={"serc-img-03"}
+            imgsrc={
+              "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_568/t_high_res_template/images/growth/home-screen/1678721769247-44fa76.jpeg"
+            }
+          />
+          <Card
+            classs={"serc-img-04"}
+            imgsrc={
+              "https://res.cloudinary.com/urbanclap/image/upload/q_auto,f_auto,fl_progressive:steep,w_568/t_high_res_template/images/growth/home-screen/1678463309015-2b92d2.jpeg"
+            }
+          />
         </div>
       </section>
     </div>
